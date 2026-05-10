87012051b901bb3aee7a1941c4db759a9c149610
ThunderboltLei <lm82_007@163.com>
2026-05-10 09:56:49 +0800

    refactor: add template scripts

---
+++ resources/types/template-prompt_type.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+# === File Description ===
+# 
+# Creator: Raymond-Magnus-Lei
+# Filename: 
+# Description: 默认样式：Ninja
+# 
+
+
+# === Function Description Format ===
+# 
+# Description: 
+# Params:
+#   param1: 
+#   param2: 
+# Result: 
+# 
+
+# 组装左提示符
+assemble_colorful_prompt() {
+    # 左侧提示符组装逻辑
+}
+
+# 组装右提示符
+assemble_colorful_prompt_right() {
+    # 右侧提示符组装逻辑
+}
+
+# 设置一个标志变量
+# 变量
+
+# 命令执行前
+preexec() {
+    # 处理逻辑
+}
+
+# 命令执行后恢复完整样式
+precmd() {
+    # 处理逻辑
+}
+
