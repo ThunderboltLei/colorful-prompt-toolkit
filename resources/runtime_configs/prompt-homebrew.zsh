421a64e778afc87c09c0604c58f45bce48788023
ThunderboltLei <lm82_007@163.com>
2026-06-14 16:03:48 +0800

    fixed: optimze homebrew configs

--- resources/runtime_configs/prompt-homebrew.zsh
+++ resources/runtime_configs/prompt-homebrew.zsh
@@ -17,7 +17,7 @@ export HOMEBREW_REPOSITORY="$(brew --repository)"
 
 # 阿里源
 export HOMEBREW_INSTALL_FROM_API=1
-export HOMEBREW_API_DOMAIN="https://mirrors.aliyun.com/homebrew-bottles/api"
 export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
-export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
+export HOMEBREW_API_DOMAIN="https://mirrors.aliyun.com/homebrew-bottles/api"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.aliyun.com/homebrew/homebrew-bottles"
+
