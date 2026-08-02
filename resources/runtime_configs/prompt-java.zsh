78bbe36a0549651dce1474d33362ee7bf6e8d1dc
ThunderboltLei <lm82_007@163.com>
2026-08-02 14:17:20 +0800

    styled: modify maven configs

--- resources/runtime_configs/prompt-java.zsh
+++ resources/runtime_configs/prompt-java.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
 ### java
-export JAVA_HOME="$MyStudios/developments/java/zulu8.94.0.17-ca.jdk/Contents/Home"
+# export JAVA_HOME="$MyStudios/developments/java/zulu8.94.0.17-ca.jdk/Contents/Home"
+export JAVA_HOME="$MyStudios/developments/java/zulu8.96.0.19-ca.jdk/Contents/Home"
 export PATH="$JAVA_HOME/bin:$PATH"
--- resources/runtime_configs/prompt-maven.zsh
+++ resources/runtime_configs/prompt-maven.zsh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
+# url: https://maven.apache.org/
+
 export MAVEN_HOME="$MyStudios/developments/maven/apache-maven-3.3.9"
+export MAVEN_REPOSITORY="$MyStudios/developments/maven/myrepository"
 export PATH="$MAVEN_HOME/bin:$PATH"
