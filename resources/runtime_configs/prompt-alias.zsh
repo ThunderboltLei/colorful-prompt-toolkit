d39186a9d9ca44cb6b79acfdd7e7ee95208fbb1a
ThunderboltLei <lm82_007@163.com>
2026-08-15 16:01:35 +0800

    style: modify alias

--- resources/runtime_configs/prompt-alias.zsh
+++ resources/runtime_configs/prompt-alias.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
 ### alias
-alias history-c='clear && history -p'
+#alias history-c='clear && history -p'
 alias brew-c='brew cleanup && brew cleanup --prune=all'
+alias conda-c='conda clean -t -y && conda clean --packages -y'
